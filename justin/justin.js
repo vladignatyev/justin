9b8a65bb773f37f4a75ce449547361b306a04649
Vladimir Ignatyev <vignatyev@flexis.ru>
2011-09-13 22:01:13 +0400

    Implementation with one simulated topic

--- justin/justin.js
+++ justin/justin.js
@@ -2,15 +2,14 @@
  * Prototype, that does all the job
  * */
 function Connection (justin, socket) {
-	var instance = this;
-	
 	this.socket = socket;
 	this.justin = justin;
-	
 	this.master = null;
+
+	var instance = this;
 	
 	this.setMaster = function (masterConnection) {
-		if (this.master != masterConnection) {
+		if (this.master !== masterConnection) {
 			if (this.master != null) {
 				this.master.socket.removeListener('data', this.handleData);
 			}
@@ -24,38 +23,29 @@ function Connection (justin, socket) {
 	};
 	
 	this.handleData = function (dataBuffer) {
-		var thisConnection = instance;
-		var data = dataBuffer;
-		
-		instance.justin.connections.forEach(function(connection, i, connections){
-			if (connection != thisConnection) {
-				connection.socket.write(data);
-			}
-		});
+		instance.socket.write(dataBuffer);
 	};
 };
 
 /**
  * Justin connection management singleton.
- * It manages connections and provide entry point for data processing loop.
  * */
 var Justin = {
 	/*Connections pool*/
 	connections: [],
 	
 	handleConnection: function (socket) {
-		var connectionObj = new Connection(this, socket);
+		var connectionObj = new Connection(Justin, socket);
+		connectionObj.setMaster(Justin.connections[0]);	// the first will rule the world
 		socket.on('end', function(){
 			Justin.closeConnection(connectionObj);
 		});
 		Justin.connections.push(connectionObj);
-		
-		connectionObj.setMaster(Justin.connections[0]);	// the first will rule the world
 	},
 	
 	closeConnection: function (connection) {
 		connection.socket.end();
-		Justin.connections.splice(Justin.pool.indexOf(connection), 1);
+		Justin.connections.splice(Justin.connections.indexOf(connection), 1);
 	}
 };
 
